61cf1f0a10a3a6a8af3c5096b0ad8d44a6e454de
JingWei-S <weijing.524@gmail.com>
2023-04-29 15:46:29 +1000

    update the design

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,25 @@
-const Card = (src) => {
-    return (
-        <div>
-            <img src={src}/>
-        </div>
-    )
-}
+import '../App.css';
+
+
+const Card = (props) => {
+  const addToCart = () => {
+    console.log("Implement add to card function later.");
+  };
+  const { toy_src, toy_id, toy_name, toy_price } = props;
+  return (
+    <div className="product-display">
+      <img
+        key={toy_id}
+        src={process.env.PUBLIC_URL + toy_src}
+        alt="A plush toy"
+      />
+      <p className='toy-name'>{toy_name}</p>
+      <p className='toy-price'>{"$" + toy_price}</p>
+      <button onClick={addToCart}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default Card;
+
+
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,7 +1,61 @@
+import Card from "./Card";
 const Shop = () => {
-    return (
-        <div>Hello from Shop page</div>
-    )
-}
+  const toys = [
+    {
+      src: "/img/Octupus 1.png",
+      id: 1,
+      name: "Tentacle Terry",
+      price: "19.99",
+    },
+    { src: "/img/Octupus 2.png", id: 2, name: "Octo Oscar", price: "19.99" },
+    {
+      src: "/img/Cthulhu 1.png",
+      id: 3,
+      name: "Huggable Horror",
+      price: "24.99",
+    },
+    {
+      src: "/img/Cthulhu 2.png",
+      id: 4,
+      name: "Whimsical Whispers",
+      price: "24.99",
+    },
+    { src: "/img/Cthulhu 3.png", id: 5, name: "Cozy Kraken", price: "24.99" },
+    { src: "/img/Owl 1.png", id: 6, name: "Wise Watchtower", price: "21.99" },
+    { src: "/img/Owl 2.png", id: 7, name: "Funny Woodsy", price: "21.99" },
+    { src: "/img/Owl 3.png", id: 8, name: "All Nighty", price: "22.99" },
+    { src: "/img/Rabbit 1.png", id: 9, name: "Bunny Bonbon", price: "16.99" },
+    {
+      src: "/img/Rabbit 2.png",
+      id: 10,
+      name: "Cinnamon Cottontail",
+      price: "16.99",
+    },
+    { src: "/img/Poodle 1.png", id: 11, name: "Pom Pom Pup", price: "27.99" },
+    {
+      src: "/img/Poodle 2.png",
+      id: 12,
+      name: "Fifi Fluffball",
+      price: "19.99",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="products">
+          {toys.map((toy) => {
+            return (
+              <Card
+                toy_src={toy.src}
+                toy_id={toy.id}
+                toy_name={toy.name}
+                toy_price={toy.price}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default Shop;
